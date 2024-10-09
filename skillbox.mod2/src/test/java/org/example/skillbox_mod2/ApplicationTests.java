0dfe9d8b701adb40c80b2d904954dd26b78a5a96
Alekseev Nikita <nakinaki243@gmail.com>
2024-10-09 16:00:14 +0500

    feat: add mod 2 skillbox

--- skillbox.mod2/src/test/java/org/example/skillbox_mod2/ApplicationTests.java
+++ skillbox.mod2/src/test/java/org/example/skillbox_mod2/ApplicationTests.java
@@ -2,12 +2,9 @@ package org.example.skillbox_mod2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootTest
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
